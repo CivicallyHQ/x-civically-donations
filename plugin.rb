e4eb50d94a24ef8f51939875916d94794e7d4b08
Angus McLeod <angus@mcleod.org.au>
2018-06-21 16:50:33 +1000

    init

---
+++ plugin.rb
@@ -0,0 +1,7 @@
+# name: civically-donations-extension
+# about: Extends the Discourse Donations plugin
+# version: 0.1
+# authors: Angus McLeod
+# url: https://github.com/civicallyhq/x-civically-donations
+
+register_asset 'stylesheets/donations-extension.scss'
